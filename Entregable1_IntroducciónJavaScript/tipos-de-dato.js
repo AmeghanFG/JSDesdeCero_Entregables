8c7d71611513bab3ee4352ce726964f612f30278
Meghan <afigueroa57@ucol.mx>
2025-03-20 21:22:11 -0600

    Entregables del 20-03-2025

---
+++ Entregable1_IntroducciónJavaScript/tipos-de-dato.js
@@ -0,0 +1,18 @@
+// Impresión de los tipos de datos
+console.log(typeof 42); // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof FALSE); // undefined
+console.log(typeof FaLsE); // undefined
+
+let variable; 
+
+console.log(typeof 45.3); // number
+console.log(typeof 300000000000000); // number
+console.log(typeof '1#@str'); // string
+console.log(typeof variable); // undefined
--- Entregable2_Comparaciones-y-decisiones-JS_18-03-25/script.js
+++ Entregable2_Comparaciones-y-decisiones-JS_18-03-25/script.js
@@ -1,12 +1,17 @@
 // Calculadora de calificaciones 
 const nota = 40;
+const estudiante = true;
 
-if (nota >= 90) {
-    console.log('Excelente, aprobaste con ' + nota);
-} else if (nota >= 75 && nota <= 89) {
-    console.log('Bien, aprobaste con ' + nota);
-} else if (nota >= 60 && nota <= 74) {
-    console.log('Suficiente, aprobaste con ' + nota);
+if (estudiante) {
+    if (nota >= 90) {
+        console.log('Excelente, aprobaste con ' + nota);
+    } else if (nota >= 75 && nota <= 89) {
+        console.log('Bien, aprobaste con ' + nota);
+    } else if (nota >= 60 && nota <= 74) {
+        console.log('Suficiente, aprobaste con ' + nota);
+    } else {
+        console.log('Tu nota fue de ' + nota + ', no aprobaste');
+    }
 } else {
-    console.log('Tu nota fue de ' + nota + ', no aprobaste');
+    console.log('¿Para qué quieres ser evaluado?')
 }
